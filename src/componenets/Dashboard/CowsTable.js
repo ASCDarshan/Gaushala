4d708a2201ab934cbd59fbea6bf7f71ba90ddb06
ASCDarshan <ascdarshan@gmail.com>
2024-12-19 15:12:33 +0530

    title and sub title color change -remove search button - add ./home in package json

--- src/componenets/Dashboard/CowsTable.js
+++ src/componenets/Dashboard/CowsTable.js
@@ -222,12 +222,6 @@ const CowsTable = () => {
               />
               <MagnifyingGlassIcon className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
             </div>
-            <button
-              type="submit"
-              className="px-4 py-2 bg-sky-500 text-white rounded-lg hover:bg-sky-600"
-            >
-              Search
-            </button>
           </form>
 
           <button
